b5146463ad863776da9b01f100f2611a71712f77
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2025-04-19 22:23:18 -0300

    backend: add a barebones backend

---
+++ src/backend/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"log"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type Star struct {
+	Name     string `json:"name"`
+	Distance int    `json:"distance"`
+	Size     string `json:"size"`
+}
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("I want a Camel Blue"))
+	})
+
+	r.Get("/api/v1/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	r.Get("/api/v1/stars", func(w http.ResponseWriter, r *http.Request) {
+		stars := []Star{
+			{Name: "Sirius", Distance: 8, Size: "Large"},
+			{Name: "Proxima Centauri", Distance: 4, Size: "Small"},
+			{Name: "Alpha Centauri", Distance: 4, Size: "Medium"},
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(stars)
+	})
+
+	log.Println("Starting server on :3000")
+	http.ListenAndServe(":3000", r)
+}
